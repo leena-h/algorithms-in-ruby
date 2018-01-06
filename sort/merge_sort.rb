e1952910ef549a709e6ba56ae29f44f4f9178b06
Leena Ha <leena@hhd.com.au>
2018-01-06 19:29:26 +1100

    add comment to merge sort

--- sort/merge_sort.rb
+++ sort/merge_sort.rb
@@ -1,3 +1,11 @@
+# Merge sort is a recursive algorithm that continually splits a list in half.
+# If the list is empty or has one item, it is sorted by definition (the base case).
+# If the list has more than one item, we split the list and recursively invoke
+# a merge sort on both halves.
+# Once the two halves are sorted, the fundamental operation, called a merge, is performed.
+
+# http://interactivepython.org/runestone/static/pythonds/SortSearch/TheMergeSort.html
+
 class MergeSort
   def sort(list)
     return list if list.size <= 1
